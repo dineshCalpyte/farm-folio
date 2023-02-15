b1efda6c74e3dd2e900b1fc5ba8ece5139509594
DESKTOP-UR9EK4O\Calpyte <dinesh.k@calpyte.com>
2023-02-15 18:25:58 +0530

    initial commit

---
+++ src/test/java/com/farmfolio/traceability/TraceabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmfolio.traceability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
